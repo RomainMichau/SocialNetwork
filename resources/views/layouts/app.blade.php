e708d615bff6f5bd948be7b319c8d513adc48324
Romain <romain9692@gmail.com>
2017-04-19 14:18:49 +0200

    changement du lien

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
                     @if(Auth::check())
-                        <li><a href="{{ url('/admin/events') }}">Chronologie</a></li>
+                        <li><a href="{{ url('/admin/mur') }}">Chronologie</a></li>
                         <li><a href="{{ url('/about') }}">A Propos</a></li>
                         <li><a href="{{ url('/admin/users') }}">Utilisateur</a></li>
                         <li><a href="{{ url('/admin/friends') }}">Amis</a></li>
