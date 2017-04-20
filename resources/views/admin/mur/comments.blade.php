c5bf0b30db9b3f1cfe890a01f893b27a0cbdf29f
Kingsun21 <t.grimault21@gmail.com>
2017-04-20 21:09:04 +0200

    kommentaires bien propres sa mère

--- resources/views/admin/mur/comments.blade.php
+++ resources/views/admin/mur/comments.blade.php
@@ -1,10 +1,17 @@
 @forelse($post->comments as $comment)
-    <div style="border-bottom: solid 1px #ddd;"> {{DB::table('users')->where('id',$comment->user_id)->get()[0]->name}}: {{ $comment->comment }}
-        @if($comment->user_id==Auth::user()->id)
-            {{ Form::open(['route' => ['admin.comments.destroy',$comment->id], 'method' => 'delete']) }}
-            {{ Form::submit('supprimer', ['class' => 'btn btn-danger btn-xs']) }} <br><br>
-            {{ Form::close() }}
-        @endif
+    <div style="border-bottom: solid 1px #ddd;"> 
+		<div class="row" style="margin: 10px 0 10px 0;">
+			<div class="col-md-9">
+				{{DB::table('users')->where('id',$comment->user_id)->get()[0]->name}}: {{ $comment->comment }}
+			</div>
+			<div class="col-md-3">
+				@if($comment->user_id==Auth::user()->id)
+					{{ Form::open(['route' => ['admin.comments.destroy',$comment->id], 'method' => 'delete']) }}
+					{{ Form::submit('supprimer', ['class' => 'btn btn-danger btn-xs']) }} 
+					{{ Form::close() }}
+				@endif
+			</div>
+		</div>
     </div>
 
 @empty
